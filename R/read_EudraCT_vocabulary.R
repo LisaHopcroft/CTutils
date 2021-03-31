5ce7d7743c61b73bb922d3945c0111ab89e8b9c1
LisaHopcroft <Lisa.Hopcroft@phs.scot>
2021-03-31 16:52:48 +0100

    Incorporating relevant EudraCT vocabulary files into the package.

---
+++ R/read_EudraCT_vocabulary.R
@@ -0,0 +1,22 @@
+#' Provide necessary EudraCT and RMS vocabulary for use when generating
+#' the EudraCT export
+#'
+#' @importFrom readxl read_excel
+#'
+#' @export
+read_EudraCT_vocabulary = function() {
+  
+  EudraCT_vocabulary = readxl::read_excel( system.file( "resources",
+                                                "EudraCT_vocabulary.xlsx",
+                                                package = "CTutils" ),
+                                   sheet="EudraCT" )
+  
+  RMS_vocabulary = readxl::read_excel( system.file( "resources",
+                                            "EudraCT_vocabulary.xlsx",
+                                            package = "CTutils" ),
+                               sheet="RMS" )
+  
+  return( list( EudraCT = EudraCT_vocabulary,
+          RMS = RMS_vocabulary ) )
+  
+}
