568723cbd33d8ed161b5290c60fa62efb63bf261
LisaHopcroft <Lisa.Hopcroft@phs.scot>
2021-04-06 10:12:39 +0100

    Updating go() to do().

---
+++ R/do.R
@@ -0,0 +1,9 @@
+#' Utility/wrapper function to DO the default things
+#'
+#' Simply sources any script called "DO.R" in the current
+#' directory.  Reduces typing for lazy people like me.
+#'
+#' @export
+do = function() {
+  source("DO.R")
+}
--- R/go.R
+++
@@ -1,9 +0,0 @@
-#' Utility/wrapper function to make things GO
-#'
-#' Simply sources any script called "GO.R" in the current
-#' directory.  Reduces typing for lazy people like me.
-#'
-#' @export
-go = function() {
-  source("GO.R")
-}
